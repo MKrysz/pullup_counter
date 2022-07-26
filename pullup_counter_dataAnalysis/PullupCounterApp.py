1ca6a8cd31e3d1e1ff4669797d338eafd83cf549
Maciej Krysztowiak <maciej.m.krysztowiak@gmail.com>
2022-07-26 15:50:21 +0200

    App: interface and dayDistribution plot added

--- pullup_counter_dataAnalysis/PullupCounterApp.py
+++ pullup_counter_dataAnalysis/PullupCounterApp.py
@@ -1,4 +1,12 @@
 from pullupSerial import PullupSerial
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import pandas as pd
+
+welcomeMsg = \
+    "For connecting to the device write connect\n"\
+    "dayDistribution plots day distribution\n"\
+    "To exit write exit\n"
 
 def ConnectAndUpdate():
     pullupCounter = PullupSerial(debug=False)
@@ -10,7 +18,39 @@ def ConnectAndUpdate():
     pullupCounter.PortDisconnect()
     return 0
 
+def plotDayDistribution():
+    data = pd.read_table('data.txt', delimiter = ' ')
+    temp = data.groupby(["Hour"]).count()["Id"]
+    N = len(data)
+
+    plotData = []
+    i = 0
+    for hour, val in temp.iteritems():
+        while(i < hour):
+            plotData.append(0)
+            i+=1
+        plotData.append(val/N)
+        i+=1
+
+    plt.plot(plotData)
+    plt.xlabel("Hours")
+    plt.ylabel("Pullups")
+    plt.title("Pullups distribution during a day")
+    plt.grid('on')
+    plt.xticks([i for i in range(24)])
+    plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1.0))
+    plt.show()
+
 def main():
+    while(1):
+        cmd = input(welcomeMsg)
+        if cmd == "exit":
+            break
+        elif(cmd == "connect"):
+            ConnectAndUpdate()
+        elif(cmd == "dayDistribution"):
+            plotDayDistribution()
+        
     return 0
 
 if __name__ == '__main__':
